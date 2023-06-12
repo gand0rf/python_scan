460b5349ed9b25ccaeb03b3b2a83c53eda380245
Gand0rf <gand0rf@protonmail.com>
2023-06-12 19:38:35 -0400

    implamented new scanning method

--- ip_scan.py
+++ ip_scan.py
@@ -1,55 +1,31 @@
-import subprocess, time, multiprocessing, psutil
+import subprocess, time, multiprocessing
 
-def ping(nums):
-    alive = []
-    for i in range(nums[0], nums[1]+1, 1):
-        check = subprocess.Popen(['ping', f'192.168.1.{i}', '-c', '1'], stdout=subprocess.PIPE)
-        result = check.communicate()
-        if b'1 received' in result[0]:
-            alive.append(f'192.168.1.{i}')
-    return alive
+def ping(num):
+    alive_ip = ''
+    check = subprocess.Popen(['ping', f'192.168.1.{num}', '-c', '1'], stdout=subprocess.PIPE)
+    result = check.communicate()
+    if b'1 received' in result[0]:
+        alive_ip = f'192.168.1.{num}'
+    return alive_ip
 
 def ips(ip_list):
     found_ips = []
-    for x in ip_list:
-        if x != '':
-            for y in x:
-                found_ips.append(y)
+    for ip in ip_list:
+        if ip != '':
+            found_ips.append(ip)
     return found_ips
 
-def num_fill(cores):
-    num_list = []
-    c = 0
-    num_per_block = 256//cores
-    while c <= 254:
-        if c == 0:
-            num_list.append((c, c + num_per_block))
-            c = c + num_per_block
-        elif (c + num_per_block) >= 256:
-            num_list.append((c + 1, 254))
-            c = 256
-        else:
-            num_list.append((c + 1, c + num_per_block))
-            c = c + num_per_block
-
-    return num_list
-
-def core_scan(numbers):
+def core_scan():
     print('\nStarting core scans...')
     start_time = time.perf_counter()
     with multiprocessing.Pool() as pool:
-        results = pool.map(ping, numbers)
+        results = pool.map(ping, (num for num in range(0, 255)))
     end_time = time.perf_counter()
     print('\nCore scans completed...')
-
     return ips(results), (end_time - start_time)
 
 if __name__ == '__main__':
-    cores = psutil.cpu_count()
-    numbers = num_fill(cores)
-
-    print(f'\nNumber of cores: {cores}')
-    core_scan_results = core_scan(numbers)
+    core_scan_results = core_scan()
     print('\nAlive addresses:')
     for z in core_scan_results[0]:
         print(f'\t{z}')
