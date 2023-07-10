03ec5d6727ddf0280d9c864488d248bd1c853ac4
Gand0rf <gand0rf@protonmail.com>
2023-07-09 20:48:48 -0400

    Changed to threadpool for handling processes.

--- ip_scan.py
+++ ip_scan.py
@@ -1,9 +1,9 @@
-import subprocess, time, multiprocessing, psutil
+import time, psutil, subprocess
+from multiprocessing.pool import ThreadPool
 
-def ping(two_args):
+def ping(card, num):
     alive_ip = ''
-    card, num = two_args.split('_')
-    check = subprocess.Popen(['ping', '-c', '1', '-I', f'{card}', f'192.168.1.{num}'], stdout=subprocess.PIPE)
+    check = subprocess.Popen(['ping', '-c', '1', '-I', f'{card}', f'192.168.1.{num}'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     result = check.communicate()
     if b'1 received' in result[0]:
         alive_ip = f'192.168.1.{num}'
@@ -16,31 +16,38 @@ def ips(ip_list):
             found_ips.append(ip)
     return found_ips
 
-def core_scan(card):
-    print('\nStarting core scans...')
+def thread_scan(card):
+    print('\nStarting thread scan...')
     args = []
-    start_time = time.perf_counter()
     for num in range(0,255):
-        args.append(card+'_'+str(num))
-    with multiprocessing.Pool(processes=8) as pool:
-        results = pool.map(ping, args)
+        args.append((card, str(num)))
+    start_time = time.perf_counter()
+    with ThreadPool() as pool:
+        results = pool.starmap_async(ping, args)
+        results.wait()
+        pool.close()
+        pool.join()
     end_time = time.perf_counter()
-    print('\nCore scans completed...')
-    return ips(results), (end_time - start_time)
+    print('\nThread scans completed...')
+    return ips(results.get()), (end_time - start_time)
 
 def interface():
     net_interfaces = psutil.net_if_addrs()
     net_list = list(net_interfaces.keys())
     for network in range(0, len(net_list)):
-        print(f'{network}: {net_list[network]}')
-    net_selection = int(input("Enter number for network interface to use: "))
+        if net_list[network] != 'lo':
+            print(f'{network}: {net_list[network]}')
+    net_selection = int(input("\nEnter number for network interface to use: "))
     return net_list[net_selection] 
 
 if __name__ == '__main__':
+    print('\n')
     card = interface()
-    core_scan_results = core_scan(card)
-    print('\nAlive addresses:')
-    for z in core_scan_results[0]:
-        print(f'\t{z}')
-    print(f'\nTotal time for scans: {core_scan_results[1]}')
-
+    thread_scan_results = thread_scan(card)
+    if len(thread_scan_results[0]) > 0:
+        print('\nFound addresses:\n')
+        for z in thread_scan_results[0]:
+            print(f'{z}')
+    else:
+        print('\nNo ip addresses found...')
+    print(f'\nTotal time for thread scans: {thread_scan_results[1]}\n')
